2f2ced3e7bcbc955254cbede1e6682e4f640cfd3
anwarbashir <anwar.bashir@gmail.com>
2017-08-07 13:47:55 +0100

    Update i2ctest.cpp
    Some comments updated

--- i2ctest.cpp
+++ i2ctest.cpp
@@ -9,8 +9,8 @@ int main()
 	int fd, result;
 	
 	// Initialize the interface by giving it an external device ID
-	// The MCP defaults to address 0x60
-	//
+	// Running the command 'ic2detect -y 1' reveals the device address 57 and 0x57 reveals init result 3
+	// **need to check is this is correct before proceeding because the program reveals the same result irrespective of power
 	// It returns a standard file descriptor
 	
 	fd = wiringPiI2CSetup(0x57);
